1caf24d3980bd426b79f61ad3341ead8e65e9d3c
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2021-10-15 10:11:22 -0700

    Create calcuator implementation

---
+++ IPCalculatorConsoleApp/IPCalculatorConsoleApp/IPCalculator.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPCalculatorConsoleApp
+{
+    public class IPCalculator
+    {
+        private BitArray[] ipAdressOctets;
+        private BitArray[] networkMaskOctets;
+
+        public IPCalculator()
+        {
+            ipAdressOctets = new BitArray[4];
+
+            for (int i = 0; i < ipAdressOctets.Length; i++)
+            {
+                ipAdressOctets[i] = new BitArray(8);
+            }
+
+            networkMaskOctets = new BitArray[4];
+
+            for (int i = 0; i < networkMaskOctets.Length; i++)
+            {
+                networkMaskOctets[i] = new BitArray(8);
+            }
+        }
+
+        public BitArray[] GetNetworkAdress()
+        {
+            BitArray[] result = new BitArray[4];
+
+            for (int i = 0; i < ipAdressOctets.Length; i++)
+            {
+                result[i] = new BitArray(8);
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (networkMaskOctets[i][j])
+                    {
+                        result[i][j] = ipAdressOctets[i][j];
+                    }
+                    else
+                    {
+                        result[i][j] = false;
+                    }
+                }
+            }
+
+            return result;
+        }
+        public BitArray[] GetHostMin()
+        {
+            BitArray[] result = GetNetworkAdress();
+
+            for(int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if(!networkMaskOctets[i][j])
+                    {
+                        result[i][j] = false;
+                    }
+                }
+            }
+
+            result[3][7] = true;
+
+            return result;
+        }
+
+        public BitArray[] GetHostMax()
+        {
+            BitArray[] result = GetNetworkAdress();
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (!networkMaskOctets[i][j])
+                    {
+                        result[i][j] = true;
+                    }
+                }
+            }
+
+            result[3][7] = false;
+
+            return result;
+        }
+
+        public void SetIpAddres(BitArray[] ipAdress)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                ipAdressOctets[i] = new BitArray(ipAdress[i]); 
+            }
+        }
+
+        private BitArray IntToBitArray(int value)
+        {
+            if(value > 255 | value < 0)
+            {
+                throw new InvalidOperationException("Vaue must be value > 255 | value < 0.");
+            }
+
+            BitArray result = new BitArray(8);
+
+            int i = 7;
+            while(value != 1)
+            {
+                
+                if (value % 2 == 1)
+                {
+                    result[i] = true;
+                }
+                i--;
+
+                value /= 2;
+            }
+            return result;
+        }
+
+        public void SetIpAddres(int[] ipAdress)
+        {
+            for (int i = 0; i < ipAdress.Length; i++)
+            {
+                ipAdressOctets[i] = IntToBitArray(ipAdress[i]);
+            }
+        }
+
+        public void SetNetworkMask(BitArray[] networkMask)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                networkMaskOctets[i] = new BitArray(networkMask[i]);
+            }
+        }
+
+        public void SetNetworkMask(int[] networkMask)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                networkMaskOctets[i] = IntToBitArray(networkMask[i]);
+            }
+        }
+
+        private BitArray StringToBitArray(string value)
+        {
+            BitArray result = new BitArray(8);
+
+            for(int i = 0; i < value.Length; i++)
+            {
+                if(value[i] == '0')
+                {
+                    result[i] = false;
+                }
+
+                if (value[i] == '1')
+                {
+                    result[i] = true;
+                }
+            }
+
+            return result;
+        }
+
+
+    }
+}
--- IPCalculatorConsoleApp/IPCalculatorConsoleApp/Program.cs
+++ IPCalculatorConsoleApp/IPCalculatorConsoleApp/Program.cs
@@ -1,2 +1,136 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+
+namespace IPCalculatorConsoleApp
+{
+    class IPCalculatorDemo
+    {
+        public static int[] StringToIntArray(string value)
+        {
+            int[] result = new int[4];
+
+            string[] values = value.Split('.');
+
+            for (int i = 0; i < 4; i++)
+            {
+                result[i] = int.Parse(values[i]);
+            }
+
+            return result;
+        }
+
+        public static BitArray[] StringToBitArray(string value)
+        {
+            BitArray[] result =new BitArray[4];
+            for (int i = 0; i < 4; i++)
+            {
+                result[i] = new BitArray(8);
+            }
+
+            string[] values = value.Split('.');
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (values[i][j] == '1')
+                    {
+                        result[i][j] = true;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static string BitArrayToString(BitArray value)
+        {
+            string result = string.Empty;
+
+            for(int i = 0;i < value.Length; i++)
+            {
+                if (value[i])
+                {
+                    result += "1";
+                }
+                else
+                {
+                    result += "0";
+                }
+            }
+
+            return result;
+        }
+        static void Main()
+        {
+
+            IPCalculator calculator = new IPCalculator();
+
+            bool Binary = false;
+
+            if(Binary)
+            {
+                string ip = "11000000.10101000.00000001.00011011";
+                string mask = "11111111.11111111.11111111.00000000";
+
+                BitArray[] ipInput = StringToBitArray(ip);
+                BitArray[] maskInput = StringToBitArray(mask);
+
+
+                calculator.SetIpAddres(ipInput);
+                calculator.SetNetworkMask(maskInput);
+
+                Console.WriteLine("IP:\t\t " + ip);
+                Console.WriteLine("Mask:\t\t " + mask);
+            }
+            else
+            {
+                string ip = "192.198.35.12";
+                string mask = "255.255.255.0";
+
+                int[] ipInput = StringToIntArray(ip);
+                int[] maskInput = StringToIntArray(mask);
+
+
+                calculator.SetIpAddres(ipInput);
+                calculator.SetNetworkMask(maskInput);
+
+                Console.WriteLine("IP:\t\t " + ip);
+                Console.WriteLine("Mask:\t\t " + mask);
+            }
+
+
+
+            BitArray[] NetworkAdress = calculator.GetNetworkAdress();
+            BitArray[] HostMin = calculator.GetHostMin();
+            BitArray[] HostMax = calculator.GetHostMax();
+
+
+
+            Console.Write("Network Adress:\t ");
+            foreach (BitArray i in NetworkAdress)
+            {
+                Console.Write(BitArrayToString(i) + ".");
+            }
+
+            Console.Write('\n');
+
+            Console.Write("HostMin:\t ");
+            foreach (BitArray i in HostMin)
+            {
+                Console.Write(BitArrayToString(i) + ".");
+            }
+
+            Console.Write('\n');
+
+            Console.Write("HostMax:\t ");
+            foreach (BitArray i in HostMax)
+            {
+                Console.Write(BitArrayToString(i) + ".");
+            }
+
+            Console.Write('\n');
+          
+
+        }
+    }
+}
